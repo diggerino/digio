b7bbbc29e2ff4c74275cd1d52be0a1bfd9b89c8b
mythern <myth@overflow.no>
2014-08-04 03:05:01 +0200

    Fix an error in tools, add debugprint support for string array

--- test/prints.js
+++ test/prints.js
@@ -2,6 +2,7 @@ var tools = require('../lib/tools')
 
 tools.print('Tihi')
 tools.print_table(['Tihi', 'hoho', 'haha'])
+tools.success('So i guess you could say things are getting pretty serious...')
 tools.warning('Oioi!')
 tools.error('Oh shit.')
 tools.debug('prints.js', 'test debug', 'LA-8-PV')
