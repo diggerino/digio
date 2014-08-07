b30c677348424077c6347e83733b97515b8847e6
Tri M. Nguyen <mail@trimn.net>
2014-08-07 05:29:00 +0200

    Convert prints from tools to print module

--- test/readandwriteconf.js
+++ test/readandwriteconf.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-var tools = require('../lib/tools')
+var print = require('../print')
+, tools = require('../lib/tools')
 
 tools.load_conf(function (err, conf) {
-    if (err) return tools.error(err)
-    tools.debug('tools.js', 'load_conf', JSON.stringify(conf, null, 4))
+    if (err) return print.error(err)
+    print.debug('tools.js', 'load_conf', JSON.stringify(conf, null, 4))
 
     var new_key = "bca03c6b04bde731f2080d96c79cb54e1468a994b99c981c190dd6dc9ad9d503"
     var new_name = "testkey"
@@ -17,9 +18,9 @@ tools.load_conf(function (err, conf) {
         conf.used = conf.names.indexOf('testkey')
 
         tools.write_conf(conf, function (err, data) {
-            if (err) return tools.error('Unable to write to config file!')
-            tools.success('Config file updated.')
-            tools.debug('tools.js', 'write_conf', JSON.stringify(data, null, 4))
+            if (err) return print.error('Unable to write to config file!')
+            print.success('Config file updated.')
+            print.debug('tools.js', 'write_conf', JSON.stringify(data, null, 4))
         })
     }
 })
