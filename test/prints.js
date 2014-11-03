cc67a1716d7c15ef9547c62326682ce1cf4aeed9
mythern <myth@overflow.no>
2014-11-03 18:43:26 +0100

    Remove unused test folder

--- test/prints.js
+++
@@ -1,9 +0,0 @@
-var print = require('../print')
-, tools = require('../lib/tools')
-
-print.list(['Tihi', 'hoho', 'haha'])
-print.success('So i guess you could say things are getting pretty serious...')
-print.warning('Oioi!')
-print.error('Oh shit.')
-print.debug('prints.js', 'test debug', 'LA-8-PV')
-print.debug('prints.js', 'test debug array', ['LA-8-PV', 'Moar linez', '<EOF>'])
